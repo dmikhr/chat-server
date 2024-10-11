e571c7f54c3581103e0e14e1b6cc180a052f45af
Dmitrii Khramtsov <dmitrypkh@gmail.com>
2024-10-11 10:11:00 +0300

    fixes

--- cmd/main.go
+++ cmd/main.go
@@ -6,32 +6,17 @@ import (
 	"log"
 	"net"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	desc "github.com/dmikhr/chat-server/pkg/chat_v1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 const grpcPort = 50051
 
 type server struct {
 	desc.UnimplementedChatV1Server
-}
-
-func (s *server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	log.Printf("Usernames: %v", req.Usernames)
-	return &desc.CreateResponse{}, nil
-}
-
-func (s *server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	log.Printf("Chat to delete id: %d", req.Id)
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) SendMessage(_ context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
-	log.Printf("Message from: %s | text: %s | time: %v", req.From, req.Text, req.Timestamp)
-	return &emptypb.Empty{}, nil
 }
 
 func main() {
@@ -49,4 +34,24 @@ func main() {
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+// Create - создание нового чата для пользователей req.Usernames
+// возвращает id чата
+func (s *server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("UserId: %v | Chat name: %v", req.GetUserid(), req.GetName())
+	return &desc.CreateResponse{}, nil
+}
+
+// Delete - удаление чата по id
+func (s *server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Chat to delete id: %d", req.GetId())
+	return &emptypb.Empty{}, nil
+}
+
+// SendMessage - отправка сообщения
+// From - автор сообщения, Text - текст сообщения, Timestamp - время отправки
+func (s *server) SendMessage(_ context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("Message from: %s | text: %s | time: %v", req.GetFrom(), req.GetText(), req.GetTimestamp())
+	return &emptypb.Empty{}, nil
 }
